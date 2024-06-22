b65567e740ed290416c1d5b1a741e1186437dca6
Rusakov Aleksey <rusal@bk.ru>
2024-06-22 22:24:48 +0300

    first commit

---
+++ src/router/routes.js
@@ -0,0 +1,37 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') }
+    ]
+  },
+  {
+    path: '/calc_params',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/CalcParamPage.vue') }
+    ]
+  },
+  {
+    path: '/extract',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/ExtractPage.vue') }
+    ]
+  },
+  {
+    path: '/about',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/AboutPage.vue') }
+    ]
+  },
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
